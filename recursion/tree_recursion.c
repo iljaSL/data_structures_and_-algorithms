cf2eb52208ce93076984df1c360a224ca7b349f5
Ilja Smelich <ismelich@c2r6p7.hive.fi>
2020-09-01 10:09:34 +0300

    tree recursion added

---
+++ recursion/tree_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void tree(int n)
+{
+    if (n > 0)
+    {
+        printf("%d ", n);
+        tree(n - 1);
+        tree(n - 1);
+    }
+}
+
+int main()
+{
+    tree(3);
+    return 0;
+}
